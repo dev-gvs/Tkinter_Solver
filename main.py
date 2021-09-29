58408c9b5b0cccdc1351c2c34541bf15f90d41b1
Valentine G <44653764+dev-gvs@users.noreply.github.com>
2021-09-29 12:14:32 +0600

    Update main.py

--- main.py
+++ main.py
@@ -107,7 +107,30 @@ class App:
         entry.focus()
 
 
+def center(window: Tk):
+    """
+    Centers a Tkinter window
+    :param window: the main window or Toplevel window to center
+    """
+    window.update_idletasks()
+    width = window.winfo_width()
+    frm_width = window.winfo_rootx() - window.winfo_x()
+    win_width = width + 2 * frm_width
+    height = window.winfo_height()
+    titlebar_height = window.winfo_rooty() - window.winfo_y()
+    win_height = height + titlebar_height + frm_width
+    x = window.winfo_screenwidth() // 2 - win_width // 2
+    y = window.winfo_screenheight() // 2 - win_height // 2
+    window.geometry('{}x{}+{}+{}'.format(width, height, x, y))
+    window.deiconify()
+
+
 if __name__ == '__main__':
     root = Tk()
+    root.attributes('-alpha', 0)
+
     App(root)
+    center(root)
+
+    root.attributes('-alpha', 1)
     root.mainloop()
